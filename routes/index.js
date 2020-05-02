a68cdd3b13365f0d6171188c198b5ad2332b517c
Meo <ngkimnhatnam@gmail.com>
2020-05-02 16:08:30 +0000

    fix escape & in unit name pt.2

--- routes/index.js
+++ routes/index.js
@@ -53,7 +53,7 @@ router.post("/register", function(req,res){
 			if(err){
 				req.flash("error", err.message);
 				console.log(err);
-				return res.render("register");
+				res.redirect("/register");
 			}
 			passport.authenticate("local")(req,res,function(){
 				req.flash("success", "Account successfully created");
